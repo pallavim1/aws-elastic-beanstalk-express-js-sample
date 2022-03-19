41e82a8eccefbfcd572b754b73243f85d629002f
Pallavi A Mahajan <localadmin@pallavi-mackbookpro-x86.local>
2022-03-19 16:49:40 -0700

    Full pipleline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! - Progress interview!'));
+app.get('/', (req, res) => res.send('Hello World! - Progress interview! - testing the full pipeline in AWS`'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
